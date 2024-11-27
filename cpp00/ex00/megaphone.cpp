3d4f68eb886185d44394ee500e0bb4ea5510024f
Skyrider <skyr1d3r@Rajas-Air.fritz.box>
2024-11-27 01:10:05 +0100

    ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpandipe <rpandie@student.42luxembourg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/27 00:24:23 by rpandipe          #+#    #+#             */
+/*   Updated: 2024/11/27 01:03:19 by rpandipe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j] != 0; j++)
+				std::cout << static_cast<char>(toupper(static_cast<unsigned char>(argv[i][j])));
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
+}
